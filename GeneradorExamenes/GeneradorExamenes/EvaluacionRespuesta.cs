0f3abc34ee23254d6a962ad90d5a35f7593c6de3
Roberto <rfll@tecnicoecuador.edu.ec>
2022-02-05 09:13:35 -0500

    Version 2

---
+++ GeneradorExamenes/GeneradorExamenes/EvaluacionRespuesta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneradorExamenes
+{
+    public class EvaluacionRespuesta
+    {
+        public static string Evaluar(bool SeleccionUsuario, bool OpcionCorrecta)
+        {
+            if (SeleccionUsuario==OpcionCorrecta)
+            {
+                return "Correcto";
+            }
+            return "Incorrecto";
+        }
+    }
+}
--- GeneradorExamenes/GeneradorExamenes_Test/UnitTest1.cs
+++ GeneradorExamenes/GeneradorExamenes_Test/UnitTest1.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using Xunit;
+using GeneradorExamenes;
 
 namespace GeneradorExamenes_Test
 {
